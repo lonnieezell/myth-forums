e8c2aeff630fa6c07cad1f25458a6a0b7812d5c5
Lonnie Ezell <lonnieje@gmail.com>
2019-09-28 00:07:19 -0500

    Getting a start on viewing a discussion

--- app/Views/topics/discussion.php
+++ app/Views/topics/discussion.php
@@ -2,6 +2,18 @@
 
 <?= $this->section('content') ?>
 
-    <?= d($topic) ?>
+    <div class="topic-header">
+        <h1 class="topic-title"><?= $topic->title ?></h1>
+
+        <div class="meta">
+            By Author Name on <?= $topic->created_at->format('M j, Y') ?>
+        </div>
+    </div>
+
+    <div class="topic">
+        
+
+        <?= $topic->html ?? $topic->body ?>
+    </div>
 
 <?= $this->endSection() ?>
