104d7411b5ca76bb9f625ee16c11b8dc094ea54f
Lonnie Ezell <lonnieje@gmail.com>
2019-10-08 00:16:41 -0500

    Show avatar on user profile.

--- app/Views/users/show.php
+++ app/Views/users/show.php
@@ -2,16 +2,22 @@
 
 <?= $this->section('content') ?>
 
-    <div class="jumbotron">
-        <h1><?= $user->username ?></h1>
+    <div class="jumbotron row">
 
-        <?php if (! empty($user->ageString)) : ?>
-            <p class="lead"><?= esc($user->ageString) ?></p>
-        <?php endif ?>
+        <div class="col-sm-2">
+            <img src="<?= $user->avatar() ?>" alt="<?= esc($user->username) ?>" class="avatar avatar-lg">
+        </div>
+        <div class="col">
+            <h1><?= $user->username ?></h1>
 
-        <?php if (! empty($user->locationString)) : ?>
-            <p class="lead"><?= esc($user->locationString()) ?></p>
-        <?php endif ?>
+            <?php if (! empty($user->ageString)) : ?>
+                <p class="lead"><?= esc($user->ageString) ?></p>
+            <?php endif ?>
+
+            <?php if (! empty($user->locationString())) : ?>
+                <p class="lead"><?= esc($user->locationString()) ?></p>
+            <?php endif ?>
+        </div>
     </div>
 
 <?= $this->endSection() ?>
